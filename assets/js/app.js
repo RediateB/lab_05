4662f045514c00cc54e068a310cc2a4f838b88b7
RediateB <50769628+RediateB@users.noreply.github.com>
2021-02-01 00:11:34 +0300

    sorting with ascending and scending is added

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,30 @@ const filter = document.querySelector("#filter"); //the task filter text field
 const taskList = document.querySelector(".collection"); //The ul
 const clearBtn = document.querySelector(".clear-tasks"); //the all task clear button
 //the reload button at the top right of navigation
-const reloadIcon = document.querySelector('.fa');   
+const reloadIcon = document.querySelector(".fa");
+const ascend = document.querySelector("#ascend");
+const descend = document.querySelector("#descend");
+
+var input = [];
 
 // Add Event Listener [Form , clearBtn and filter search input ]
 // form submit
+ascend.addEventListener("click", ascending);
+
+descend.addEventListener("click", descending);
+
 form.addEventListener("submit", addNewTask);
 // Clear All Tasks
 clearBtn.addEventListener("click", clearAllTasks);
 // Filter Task
 filter.addEventListener("keyup", filterTasks);
 
-// Event Listener for reload 
-reloadIcon.addEventListener('click', reloadPage);
+// Event Listener for reload
+reloadIcon.addEventListener("click", reloadPage);
+
+taskList.addEventListener("click", removeTask);
 // Event Handlers
+
 function addNewTask(e) {
   e.preventDefault();
 
@@ -26,53 +37,96 @@ function addNewTask(e) {
     return;
   }
 
-  // Create an li element when the user adds a task
+  ascending();
+
   const li = document.createElement("li");
-  // Adding a class
   li.className = "collection-item";
-  // Create text node and append it
   li.appendChild(document.createTextNode(taskInput.value));
-  // Create new element for the link
   const link = document.createElement("a");
-  // Add class and the x marker for a
   link.className = "delete-item secondary-content";
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append link to li
   li.appendChild(link);
-  // Append to ul
-  taskList.appendChild(li);
+
+  if (taskList == "") {
+    taskList.appendChild(li);
+  } else {
+    taskList.insertBefore(li, taskList.firstElementChild);
+  }
 
   taskInput.value = "";
+  text = { textContent: li.textContent };
+  input.push(text);
 }
-function clearAllTasks(e) {
-    while (taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild);
-    }
 
+function clearAllTasks(e) {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+  input = []
 }
 function filterTasks(e) {
-    let filterx=filter.value
-    let y=document.querySelectorAll(".collection-item")
-    y.forEach((item)=>{
-        if (item.textContent.indexOf(filterx)) {
-            item.style.display="none";
-        } else {
-            item.style.display="block";
-        }
-    })
+  let filterx = filter.value;
+  let y = document.querySelectorAll(".collection-item");
+  y.forEach((item) => {
+    if (item.textContent.indexOf(filterx)) {
+      item.style.display = "none";
+    } else {
+      item.style.display = "block";
+    }
+  });
 }
-
-taskList.addEventListener('click', removeTask);
 
 function removeTask(e) {
-    if (e.target.parentElement.classList.contains('delete-item')){
-        if (confirm('Are You Sure about that ?'))
-        {
-            e.target.parentElement.parentElement.remove();
-        }
-
-}
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure about that ?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
 function reloadPage() {
-    location.reload();
+  location.reload();
+}
+
+function ascending() {
+  var lis = taskList.children;
+
+  for (let index = 0; index < lis.length; index++) {
+    lis[index].style.display = "none";
+  }
+
+  for (let index = 0; index < input.length; index++) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(input[index].textContent));
+    const link = document.createElement("a");
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.className = "delete-item secondary-content";
+    li.appendChild(link);
+
+    if (taskList == "") {
+      taskList.appendChild(li);
+    } else {
+      taskList.insertBefore(li, taskList.firstElementChild);
+    }
+  }
+}
+
+function descending() {
+  var lis = taskList.children;
+
+  for (let index = 0; index < lis.length; index++) {
+    lis[index].style.display = "none";
+  }
+
+  for (let index = 0; index < input.length; index++) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(input[index].textContent));
+    const link = document.createElement("a");
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.className = "delete-item secondary-content";
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+  }
 }
